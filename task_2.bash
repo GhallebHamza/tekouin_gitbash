fbc0b21826654fdaed4656e3bbf11b4b1beadc63
GhallebHamza <Hamza.ghalleb@esprit.tn>
2023-05-08 16:20:44 +0200

    add task_2.bash script

---
+++ task_2.bash
@@ -0,0 +1,29 @@
+  GNU nano 7.2                                  task_2.bash
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#!/bin/bash
+
+# Display the last 10 lines of /etc/passwd
+echo "Last 10 lines of /etc/passwd:"
+tail -n 10 /etc/passwd
+
+echo ""
+
+# Display the first 10 lines of /etc/passwd
+echo "First 10 lines of /etc/passwd:"
+head -n 10 /etc/passwd
+
